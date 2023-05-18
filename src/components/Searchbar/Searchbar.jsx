f2c7ab8047872bdf3b56cc1c7c34d1cbfb3fbde6
ViktoriyaSF <kievstar01051985@gmail.com>
2023-05-18 21:08:01 +0300

    Searchbar_hook

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -9,53 +9,6 @@ import {
   StyledSearchFormInput,
 } from './StyledSearchbar';
 
-// export class Searchbar extends Component {
-//   state = {
-//     searchQuery: '',
-//     // додаємо елемент в state - контрольючий компонент
-//     // inputValue: '',
-//   };
-//   setSearchQuery = evt => {
-//     const { value } = evt.currentTarget;
-//     this.setState({ searchQuery: value.toLowerCase().trim() });
-//     //  робимо метод для цього контролюючого компонента
-//     // this.setState({ inputValue: value.toLowerCase() });
-//     // console.log(evt.currentTarget.value);
-//   };
-//   handleSubmit = evt => {
-//     evt.preventDefault();
-//     this.props.onSearch(this.state.searchQuery);
-//     // this.reset();
-//     this.setState({ searchQuery: ' ' });
-//     // чистимо контролюючий компонент
-//     this.setState({ inputValue: ' ' });
-//   };
-
-//   render() {
-//     const { searchQuery } = this.state;
-//     return (
-//       <StyledSearchbar>
-//         <StyledSearchForm onSubmit={this.handleSubmit}>
-//           <StyledSearchFormButton type="submit" onClick={this.handleClear}>
-//             <span>
-//               <FiSearch size="1.5em" />
-//             </span>
-//           </StyledSearchFormButton>
-//           <StyledSearchFormInput
-//             type="text"
-//             autocomplete="off"
-//             autofocus
-//             placeholder="Search images and photos"
-//             onChange={this.setSearchQuery}
-//             // додали значення value і прирівняли до значення компонента
-//             value={searchQuery}
-//             // value={this.state.inputValue}
-//           />
-//         </StyledSearchForm>
-//       </StyledSearchbar>
-//     );
-//   }
-// }
 export const Searchbar = ({ onSearch }) => {
   const [searchQuery, setSearchQuery] = useState('');
 
@@ -84,7 +37,6 @@ export const Searchbar = ({ onSearch }) => {
           autofocus
           placeholder="Search images and photos"
           onChange={handleSearchQuery}
-          // додали значення value і прирівняли до значення компонента
         />
       </StyledSearchForm>
     </StyledSearchbar>
